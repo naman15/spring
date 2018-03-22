bc04df13488d45a7ab95105c53efe7d9e6d2cbde
namangoyal15 <naman_goyal_94@yahoo.com>
2018-03-22 23:12:40 +0530

    introduced some interfaces

--- src/main/java/application/DrawingApp.java
+++ src/main/java/application/DrawingApp.java
@@ -1,6 +1,6 @@
 package application;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import beans.Triangle;
@@ -12,7 +12,8 @@ public class DrawingApp
 	
 	public static void main(String args[]) 
 	{
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		shape = (Triangle)context.getBean("triangle");
 		shape.draw();
 	}
--- src/main/java/beans/Triangle.java
+++ src/main/java/beans/Triangle.java
@@ -1,10 +1,18 @@
 package beans;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import interfaces.Shape;
 
-public class Triangle implements Shape 
+public class Triangle implements Shape , ApplicationContextAware,BeanNameAware,InitializingBean,DisposableBean
 {
 	private Point pointA;
+	private ApplicationContext context=null;
 	
 	public Point getPointA() {
 		return pointA;
@@ -17,6 +25,23 @@ public class Triangle implements Shape
 	public  void draw() 
 	{
 		System.out.println("Shape Drawn from Point - " + pointA);
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException 
+	{
+		this.context=applicationContext;
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("Bean Destroyed");
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Bean created");
+	}
+
+	public void setBeanName(String arg0) {
+		System.out.println("Bean ->" + arg0 +" formed");
 	}
 
 }
