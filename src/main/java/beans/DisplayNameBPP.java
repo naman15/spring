db9292bd36d4616f116ba5c10a7a3bc2c34f8472
namangoyal15 <naman_goyal_94@yahoo.com>
2018-03-24 15:30:45 +0530

    Added processor definition in spring.xml

--- src/main/java/beans/DisplayNameBPP.java
+++ src/main/java/beans/DisplayNameBPP.java
@@ -1,3 +1,4 @@
+package beans;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
@@ -5,14 +6,14 @@ public class DisplayNameBPP implements BeanPostProcessor{
 
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException 
 	{
-		
-		return null;
+		System.out.println("called after post initalization : created bean + " + beanName);
+		return bean;//neccassary
 	}
 
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException 
 	{
-	
-		return null;
+		System.out.println("called after pre initalization : created bean + " + beanName);
+		return bean;
 	}
 
 }
