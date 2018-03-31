7c50e9bf25f52c5acc2d3515fec3eaaa555aa04a
namangoyal15 <naman_goyal_94@yahoo.com>
2018-03-31 15:47:42 +0530

    wildCard + PointCut Expressions

--- src/main/java/aop/aspect/LoggingAspect.java
+++ src/main/java/aop/aspect/LoggingAspect.java
@@ -2,15 +2,25 @@ package aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect 
 {
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void loggingAdvice() 
 	{
 		System.out.println("Logging Advice Called");
 	}
+	
+	@Before("allGetters()")
+	public void securityAdvice() 
+	{
+		System.out.println("Security Advice Called");
+	}
+	
+	@Pointcut("execution(public * get*(..))")
+	public void allGetters() {};
 }
